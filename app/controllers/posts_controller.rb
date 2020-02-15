e072d4f577fcb357c95ebf85f7ffccd5409f5cbe
yohei-koba-tokyo <60637308+yohei-koba-tokyo@users.noreply.github.com>
2020-02-15 20:11:04 +0900

    minor update 2/15(sat)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,35 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
+
+  def show
+    @post = Post.find
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+  def post_params
+    params.permit(:message)
+  end
+
+
 end
